b0b7b3b619abe1791d604dec4b19bf86c493799a
Mekdelawit-21 <mekdih.2116@gmail.com>
2025-05-22 14:47:57 +0300

    product

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import React  from 'react'
 import Header from './Components/Header/Header'
 import Carousel from './Components/Carousel/CarouselEffect'
 import Category from './Components/Category/Category'
+import Product from './Components/Product/Product'
 
 function App() {
  
 
   return (
     <div>
-     <Header/>
-     <Carousel/>
-     <Category/>
+      <Header />
+      <Carousel />
+      <Category />
+      <Product />
     </div>
-  )
+  );
 }
 
 export default App
